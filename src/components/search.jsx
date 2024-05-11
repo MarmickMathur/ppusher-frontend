e3bbdcb67cefab79bb9892cf4e03de974ece0829
Shrikrishna Mathur <22je0547@iitism.ac.in>
2024-05-11 08:29:00 +0530

    ml intergration

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Search = ({ ontagchange }) => {
+const Search = ({ setterm }) => {
   const [input, setInput] = useState("lover boy");
+  const [rec, setrec] = useState([]);
 
   const onInputChange = (e) => {
     // console.log(e.target.value);
@@ -11,11 +13,17 @@ const Search = ({ ontagchange }) => {
   useEffect(
     /*cannot be an async function*/
     () => {
-      const search = () => {
-        let newarr = input.split(", ");
-        ontagchange(newarr);
+      const search = async () => {
+        let temp = [];
+        const { data } = await axios.get(
+          `https://ppushermusicsuggestion.onrender.com//autocomplete?query=${input}`
+        );
+        console.log(data);
+        for (let i = 0; i < Math.min(10, data.length); i++) {
+          temp.push(data[i]);
+        }
+        setrec(temp);
       };
-
       if (input !== "") {
         // to check if the component is first time rendered
         search();
@@ -24,17 +32,34 @@ const Search = ({ ontagchange }) => {
     [input]
     /* three options - an array , nothing, an empty array )*/
   );
+  let recs = [];
+  recs = rec.map((item, index) => {
+    return (
+      <input
+        key={index}
+        className="input p-2  rounded-b-md w-1/2 border-2 border-black"
+        value={item}
+        onClick={() => {
+          // console.log(item);
+          setterm(item);
+          setrec([]);
+          setInput("");
+        }}
+      />
+    );
+  });
 
   return (
-    <div>
+    <div className=" w-9/12">
       <form className="ui form">
         <div>
           <label className="pr-10"> enter the search term</label>
           <input
-            className="input border-2 border-black"
+            className="input p-2  rounded-t-md w-1/2 border-2 border-black"
             value={input}
             onChange={onInputChange}
           />
+          <ul className="absolute">{recs}</ul>
         </div>
       </form>
     </div>
--- src/components/topnav.jsx
+++ src/components/topnav.jsx
@@ -1,12 +1,12 @@
 import Search from "./search";
 import Authbutton from "./Login";
 
-const Topnav = ({ ontagchange }) => {
+const Topnav = ({ setterm }) => {
   return (
     <>
       <nav className="bg-white dark:bg-gray-900  w-full z-20 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Search ontagchange={ontagchange} />
+          <Search setterm={setterm} />
           <div className="bg-blue-900 text-white rounded-md p-3">
             <Authbutton />
           </div>
