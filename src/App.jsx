35449878469609d337540d09275808de98089400
Shrikrishna Mathur <22je0547@iitism.ac.in>
2024-05-11 06:21:06 +0530

    fixing some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import Leftnav from "./components/leftnav";
 import Topnav from "./components/topnav";
 import Songlist from "./components/songlist";
 import Musicsplitter from "./components/musicsplitter";
-import Authbutton from "./components/Login";
+// import Authbutton from "./components/Login";
+import { UserProvider } from "./context/user";
 import axios from "axios";
 
 function App() {
   const [tags, settags] = useState([]);
   const [song, setsong] = useState({});
-  const [user, setuser] = useState(null);
 
   useEffect(() => {
     const getuser = async () => {
       console.log("inside get user");
-      const res = await axios.get("http://localhost:8000/user", {
-        withCredentials: true,
-      });
+      const res = await axios.get(
+        "https://ppusher-backend.onrender.com/auth/google",
+        {
+          withCredentials: true,
+        }
+      );
       console.log(res);
       // console.log(res);
     };
@@ -25,23 +28,25 @@ function App() {
 
   return (
     <>
-      <div className="w-screen h-screen">
-        <div className="relative">
-          <Topnav ontagchange={settags} />
+      <UserProvider>
+        <div className="w-screen h-screen">
+          <div className="relative">
+            <Topnav ontagchange={settags} />
+          </div>
+          <div className="grid grid-cols-12 gap-0">
+            <div className="col-span-3">
+              <Leftnav ontagchange={settags} />
+            </div>
+            <div className="col-span-5">
+              <Songlist tags={tags} onsongchange={setsong} />
+            </div>
+            <div className="col-span-4 flex align-middle">
+              {/* <Musicplayer song={song} /> */}
+              <Musicsplitter song={song} />
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-12 gap-0">
-          <div className="col-span-3">
-            <Leftnav ontagchange={settags} />
-          </div>
-          <div className="col-span-5">
-            <Songlist tags={tags} onsongchange={setsong} />
-          </div>
-          <div className="col-span-4 flex align-middle">
-            {/* <Musicplayer song={song} /> */}
-            <Musicsplitter song={song} />
-          </div>
-        </div>
-      </div>
+      </UserProvider>
     </>
   );
 }
